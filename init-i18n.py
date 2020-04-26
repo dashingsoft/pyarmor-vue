6e98d82a51d4d8457972b786917d0a35ba5a367e
Jondy Zhao <jondy.zhao@gmail.com>
2020-04-26 21:42:48 +0800

    Init i18n messages

---
+++ init-i18n.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+#
+# Search the files list in the command line and print the translated
+# messages like
+#
+#    $t('xxxxx')
+#    _t('xxxxx')
+#
+# Usage:
+#
+#    python3 init-i18n.py $(find src -name *.vue)
+#
+import re
+import sys
+
+
+def build_message_table(files):
+    messages = {}
+    pat = re.compile(r'''[_$]t\(\s*[']([^']+)[']\s*\)''')
+    for name in files:
+        print('Searching %s ...' % name)
+        with open(name) as f:
+            n = 0
+            for line in f:
+                n += 1
+                for msg in re.findall(pat, line):
+                    if msg in messages:
+                        messages[msg].append((name, n))
+                    else:
+                        messages[msg] = [(name, n)]
+
+    indent = ' ' * 4
+    result = []
+    for msg, locations in messages.items():
+        for loc in locations:
+            result.append('{0}// {1} {2}'.format(indent, *loc))
+        result.append('{0}"{1}": "",'.format(indent, msg))
+        result.append('')
+    return result
+
+
+if __name__ == '__main__':
+    files = sys.argv[1:]
+    # print('Search files: %s' % files)
+
+    result = build_message_table(files)
+    print('\n'.join(result))
--- src/plugins/locale/messages-zh.js
+++ src/plugins/locale/messages-zh.js
@@ -5,7 +5,576 @@ const messages = {
     },
     "Server error: please check console output and make sure server is on": "服务器错误：请确认 pyarmor-webui 后台服务已经在运行，并检查控制台输出",
     "Could not connect to pyarmor server, make sure it runs on %1": "无法连接到后台服务：%1",
+
+    // src/components/SelectLicenseFile.vue 13
+    "Select one license file to restrict the obfuscated script": "",
+
+    // src/components/SelectLicenseFile.vue 73
+    "Default license, no any restrict": "",
+
+    // src/components/SelectLicenseFile.vue 77
+    "Do not include license file": "",
+
+    // src/components/HomeTabIndex.vue 3
+    "Home": "",
+
+    // src/components/HomeTabIndex.vue 12
+    // src/pages/ObfuscatePageWizard.vue 5
+    "Obfuscate Script Wizard": "",
+
+    // src/components/HomeTabIndex.vue 23
+    "Obfuscate With Project": "",
+
+    // src/components/HomeTabIndex.vue 34
+    // src/pages/PackPageWizard.vue 5
+    "Pack Script Wizard": "",
+
+    // src/components/HomeTabIndex.vue 45
+    "Pack With Project": "",
+
+    // src/components/HomeTabIndex.vue 56
+    "Generate Expired License": "",
+
+    // src/components/HomeTabIndex.vue 67
+    "Fixed Machine License": "",
+
+    // src/components/HomeTabIndex.vue 78
+    "Full Features License": "",
+
+    // src/components/HomeTabIndex.vue 89
+    "Register PyArmor": "",
+
+    // src/components/HomeTabIndex.vue 97
+    "Please select one regfile, for example, pyarmor-regfile-1.zip": "",
+
+    // src/components/HomeTabIndex.vue 103
+    "No this file?": "",
+
+    // src/components/HomeTabIndex.vue 108
+    "https://order.shareit.com/cart/add?vendorid=200089125&PRODUCT[300871197]=1": "",
+
+    // src/components/HomeTabIndex.vue 109
+    "Click here to purchase one": "",
+
+    // src/components/HomeTabIndex.vue 112
+    "Cancel": "",
+
+    // src/components/HomeTabIndex.vue 113
+    "Register": "",
+
+    // src/components/HomeTabIndex.vue 153
+    "Register PyArmor successfully": "",
+
+    // src/components/HomeTabIndex.vue 157
+    "regfile is empty": "",
+
+    // src/components/SelectPlatform.vue 9
+    "High Security": "",
+
+    // src/components/SelectPlatform.vue 12
+    "High Speed": "",
+
+    // src/components/SelectPlatform.vue 25
+    "Cross platform, select one or more platforms to run obfuscated scripts": "",
+
+    // src/components/HomeTabAbout.vue 3
+    "About": "",
+
+    // src/components/HomeTabAbout.vue 15
+    "PyArmor server is not running on": "",
+
+    // src/components/HomeTabAbout.vue 15
+    "Make sure the server is running, or click here to": "",
+
+    // src/components/HomeTabAbout.vue 19
+    "connect another server url": "",
+
+    // src/components/HomeTabAbout.vue 23
+    "PyArmor is a powerful tool in the field of protecting and distributing python scripts.": "",
+
+    // src/components/HomeTabAbout.vue 24
+    "The goal of PyArmor is to make Python applied to commercial application easily.": "",
+
+    // src/components/HomeTabAbout.vue 32
+    "Homepage": "",
+
+    // src/components/HomeTabAbout.vue 39
+    "https://pyarmor.readthedocs.io/en/latest/": "",
+
+    // src/components/HomeTabAbout.vue 39
+    "Documentation": "",
+
+    // src/components/HomeTabAbout.vue 46
+    "https://pyarmor.readthedocs.io/en/latest/license.html": "",
+
+    // src/components/HomeTabAbout.vue 46
+    "License &amp; Purchase": "",
+
+    // src/components/HomeTabAbout.vue 53
+    "Report Issues": "",
+
+    // src/components/HomeTabAbout.vue 60
+    "Concat": "",
+
+    // src/components/HomeTabAbout.vue 63
+    "Copyright @ 2008 - 2020 Dashingsoft Corp.": "",
+
+    // src/components/SelectFolder.vue 41
+    "Parent directory": "",
+
+    // src/components/SelectFolder.vue 45
+    "Restore init value": "",
+
+    // src/components/SelectFolder.vue 49
+    "Root path and favorite paths": "",
+
+    // src/components/SelectFolder.vue 53
+    "Create new path": "",
+
+    // src/components/SelectFolder.vue 57
+    "Delete an empty path": "",
+
+    // src/components/SelectFolder.vue 61
+    "Accept path": "",
+
+    // src/components/SelectFolder.vue 172
+    "This is top path": "",
+
+    // src/components/SelectFolder.vue 186
+    // src/pages/ProjectPageEdit.vue 218
+    "Input": "",
+
+    // src/components/SelectFolder.vue 198
+    // src/components/HomeTabLicense.vue 105
+    // src/components/HomeTabProject.vue 142
+    "Confirm": "",
+
+    // src/components/HomeTabLicense.vue 4
+    // src/components/HomeTabProject.vue 4
+    "New": "",
+
+    // src/components/HomeTabLicense.vue 6
+    // src/components/HomeTabProject.vue 6
+    "Refresh": "",
+
+    // src/components/HomeTabLicense.vue 17
+    // src/pages/LicensePageEdit.vue 27
+    "Expired": "",
+
+    // src/components/HomeTabLicense.vue 20
+    "Harddisk": "",
+
+    // src/components/HomeTabLicense.vue 23
+    "IPv4": "",
+
+    // src/components/HomeTabLicense.vue 26
+    "Mac Address": "",
+
+    // src/components/HomeTabLicense.vue 29
+    // src/pages/LicensePageEdit.vue 43
+    "Disable Restrict Mode": "",
+
+    // src/components/HomeTabLicense.vue 32
+    // src/pages/LicensePageEdit.vue 57
+    "Extra Data": "",
+
+    // src/components/HomeTabLicense.vue 35
+    "Filename": "",
+
+    // src/components/HomeTabLicense.vue 44
+    // src/components/HomeTabProject.vue 38
+    // src/pages/LicensePageEdit.vue 16
+    "Name": "",
+
+    // src/components/HomeTabLicense.vue 49
+    "Summary": "",
+
+    // src/components/HomeTabLicense.vue 56
+    "Edit this license": "",
+
+    // src/components/HomeTabLicense.vue 66
+    "Remove this license": "",
+
+    // src/components/HomeTabProject.vue 18
+    // src/pages/ProjectInputFile.vue 6
+    "Src": "",
+
+    // src/components/HomeTabProject.vue 21
+    "Entry": "",
+
+    // src/components/HomeTabProject.vue 24
+    // src/pages/PackPageWizard.vue 116
+    // src/pages/ObfuscatePageWizard.vue 35
+    // src/pages/ProjectInputTarget.vue 4
+    // src/pages/ProjectPageEdit.vue 47
+    "Output": "",
+
+    // src/components/HomeTabProject.vue 27
+    "Target": "",
+
+    // src/components/HomeTabProject.vue 30
+    "Path": "",
+
+    // src/components/HomeTabProject.vue 47
+    "Title": "",
+
+    // src/components/HomeTabProject.vue 54
+    "Build this project": "",
+
+    // src/components/HomeTabProject.vue 59
+    "Build with debug information in case something is wrong": "",
+
+    // src/components/HomeTabProject.vue 64
+    "Remove this project": "",
+
+    // src/components/HomeTabProject.vue 69
+    "Edit this project": "",
+
+    // src/components/HomeTabProject.vue 98
+    // src/pages/ProjectPageEdit.vue 32
+    "Obfuscate": "",
+
+    // src/components/HomeTabProject.vue 98
+    "One folder bundle": "",
+
+    // src/components/HomeTabProject.vue 99
+    "One file bundle": "",
+
+    // src/components/HomeTabProject.vue 99
+    "One file bundle with outer license": "",
+
+    // src/components/HomeTabProject.vue 121
+    // src/pages/ProjectPageEdit.vue 186
+    "Building ": "",
+
+    // src/components/HomeTabProject.vue 134
+    // src/pages/ProjectPageEdit.vue 199
+    "Diagnosing project ": "",
+
+    // src/pages/PackPageWizard.vue 10
+    // src/pages/ObfuscatePageWizard.vue 10
+    "Start": "",
+
+    // src/pages/PackPageWizard.vue 12
+    "Data Files": "",
+
+    // src/pages/PackPageWizard.vue 14
+    // src/pages/PackPageWizard.vue 104
+    "Extra Options": "",
+
+    // src/pages/PackPageWizard.vue 16
+    // src/pages/ObfuscatePageWizard.vue 14
+    "Finish": "",
+
+    // src/pages/PackPageWizard.vue 28
+    "Base path for entry script, data files, binary files etc.": "",
+
+    // src/pages/PackPageWizard.vue 34
+    // src/pages/ProjectInputFile.vue 27
+    "Script": "",
+
+    // src/pages/PackPageWizard.vue 36
+    "The entry script": "",
+
+    // src/pages/PackPageWizard.vue 46
+    "No obfuscated scripts in these paths": "",
+
+    // src/pages/PackPageWizard.vue 61
+    "Type module or package name, then press ENTER": "",
+
+    // src/pages/PackPageWizard.vue 62
+    "Name an import not visible in the code of the script(s)": "",
+
+    // src/pages/PackPageWizard.vue 73
+    "Additional data files or folders to be added to the executable": "",
+
+    // src/pages/PackPageWizard.vue 74
+    "Type file name or path name relative to src, then press ENTER": "",
+
+    // src/pages/PackPageWizard.vue 85
+    "Additional binary files to be added to the executable": "",
+
+    // src/pages/PackPageWizard.vue 86
+    "Type binary filename relative to src, then press ENTER": "",
+
+    // src/pages/PackPageWizard.vue 92
+    "No Console": "",
+
+    // src/pages/PackPageWizard.vue 94
+    "Do not provide a console window for standard I/O": "",
+
+    // src/pages/PackPageWizard.vue 100
+    "Apply icon to a Windows executable or the bundle on Mac OS X": "",
+
+    // src/pages/PackPageWizard.vue 109
+    "Any other PyInstaller options to append command line": "",
+
+    // src/pages/PackPageWizard.vue 118
+    // src/pages/ObfuscatePageWizard.vue 37
+    // src/pages/ProjectInputTarget.vue 6
+    "The default output path is $src/dist": "",
+
+    // src/pages/PackPageWizard.vue 124
+    "Bundle": "",
+
+    // src/pages/PackPageWizard.vue 126
+    "Name to the bundled app (default: basename of entry script)": "",
+
+    // src/pages/PackPageWizard.vue 132
+    "all to one folder": "",
+
+    // src/pages/PackPageWizard.vue 133
+    "all to one file": "",
+
+    // src/pages/PackPageWizard.vue 134
+    "all to one file with outer license": "",
+
+    // src/pages/PackPageWizard.vue 153
+    "Pack": "",
+
+    // src/pages/PackPageWizard.vue 161
+    "Close": "",
+
+    // src/pages/PackPageWizard.vue 168
+    "Prev": "",
+
+    // src/pages/PackPageWizard.vue 174
+    "Next": "",
+
+    // src/pages/PackPageWizard.vue 282
+    "Packing script ": "",
+
+    // src/pages/ProjectInputMisc.vue 4
+    "Restrict Mode": "",
+
+    // src/pages/ProjectInputMisc.vue 7
+    "https://pyarmor.readthedocs.io/en/latest/mode.html#restrict-mode": "",
+
+    // src/pages/ProjectInputMisc.vue 11
+    "Select restrict mode": "",
+
+    // src/pages/ProjectInputMisc.vue 21
+    "Bootstrap Code": "",
+
+    // src/pages/ProjectInputMisc.vue 25
+    "Select entry mode": "",
+
+    // src/pages/ProjectInputMisc.vue 35
+    "Enable Suffix": "",
+
+    // src/pages/ProjectInputMisc.vue 38
+    "Generate runtime package with an unique suffix": "",
+
+    // src/pages/ProjectInputMisc.vue 60
+    "DO NOT insert bootstrap code into entry scripts": "",
+
+    // src/pages/ProjectInputMisc.vue 64
+    "Insert bootstrap code into entry scripts": "",
+
+    // src/pages/ProjectInputMisc.vue 68
+    "Generate bootstrap code without leading dot (absolute import)": "",
+
+    // src/pages/ProjectInputMisc.vue 72
+    "Generate bootstrap code with leading dot (relative import)": "",
+
+    // src/pages/ProjectInputMisc.vue 78
+    "Mode 0, disable all the restricts for the obfuscated scripts": "",
+
+    // src/pages/ProjectInputMisc.vue 82
+    "Mode 1": "",
+
+    // src/pages/ProjectInputMisc.vue 86
+    "Mode 2": "",
+
+    // src/pages/ProjectInputMisc.vue 90
+    "Mode 3": "",
+
+    // src/pages/ProjectInputMisc.vue 94
+    "Mode 4": "",
+
+    // src/pages/LicenseInputMachine.vue 6
+    "Bind obfuscated scripts to this serial number of harddisk": "",
+
+    // src/pages/LicenseInputMachine.vue 13
+    "Bind obfuscated scripts to this IPv4 address": "",
+
+    // src/pages/LicenseInputMachine.vue 20
+    "Bind obfuscated scripts to this MAC address": "",
+
+    // src/pages/ObfuscatePageWizard.vue 12
+    "Advanced": "",
+
+    // src/pages/ObfuscatePageWizard.vue 26
+    // src/pages/ProjectInputMode.vue 9
+    "Cross Protection": "",
+
+    // src/pages/ObfuscatePageWizard.vue 28
+    // src/pages/ProjectInputMode.vue 11
+    "Inject cross protection code into entry scripts before obfuscating": "",
+
+    // src/pages/ObfuscatePageWizard.vue 44
+    "Package Name": "",
+
+    // src/pages/ObfuscatePageWizard.vue 48
+    "Append this name to output path": "",
+
+    // src/pages/ObfuscatePageWizard.vue 57
+    // src/pages/ProjectInputTarget.vue 32
+    "Platforms": "",
+
+    // src/pages/ObfuscatePageWizard.vue 63
+    // src/pages/ProjectInputTarget.vue 25
+    "License": "",
+
+    // src/pages/ObfuscatePageWizard.vue 193
+    "Obfuscate scripts: ": "",
+
+    // src/pages/ProjectInputMode.vue 3
+    "Advanced Mode": "",
+
+    // src/pages/ProjectInputMode.vue 5
+    "Change code object structure to improve security (only for x86/64 arch)": "",
+
+    // src/pages/ProjectInputMode.vue 15
+    "Obfuscate Module": "",
+
+    // src/pages/ProjectInputMode.vue 17
+    "Obfuscate the whole module": "",
+
+    // src/pages/ProjectInputMode.vue 21
+    "Obfuscate Code Object": "",
+
+    // src/pages/ProjectInputMode.vue 23
+    "Obfuscate each function (code object) in the module": "",
+
+    // src/pages/ProjectInputMode.vue 27
+    "Wrap Code Object": "",
+
+    // src/pages/ProjectInputMode.vue 29
+    "Obfuscate each function (code object) in runtime": "",
+
+    // src/pages/ProjectInputTarget.vue 13
+    "Bundle Name": "",
+
+    // src/pages/ProjectInputTarget.vue 39
+    "Plugins": "",
+
+    // src/pages/ProjectInputTarget.vue 52
+    "Type plugin name, then press ENTER": "",
+
+    // src/pages/ProjectInputTarget.vue 61
+    "Pack Options": "",
+
+    // src/pages/ProjectInputTarget.vue 66
+    "Please input any pyinstaller option": "",
+
+    // src/pages/ProjectInputTarget.vue 86
+    "Name to assign to the bundled app (default: entry script’s basename)": "",
+
+    // src/pages/ProjectInputTarget.vue 87
+    "Append this name to output path for package": "",
+
+    // src/pages/ProjectPageEdit.vue 6
+    // src/pages/ProjectPageEdit.vue 8
+    "Edit Project": "",
+
+    // src/pages/ProjectPageEdit.vue 6
+    // src/pages/ProjectPageEdit.vue 9
+    "New Project": "",
+
+    // src/pages/ProjectPageEdit.vue 26
+    "Basic": "",
+
+    // src/pages/ProjectPageEdit.vue 27
+    "Type": "",
+
+    // src/pages/ProjectPageEdit.vue 35
+    "Pack all to one folder": "",
+
+    // src/pages/ProjectPageEdit.vue 38
+    "Pack all to one file": "",
+
+    // src/pages/ProjectPageEdit.vue 41
+    "Pack all to one file with outer license": "",
+
+    // src/pages/ProjectPageEdit.vue 50
+    "Obfuscate Mode": "",
+
+    // src/pages/ProjectPageEdit.vue 53
+    "Advanced Options": "",
+
+    // src/pages/ProjectPageEdit.vue 56
+    "Runtime Files": "",
+
+    // src/pages/ProjectPageEdit.vue 143
+    "DO NOT generate runtime files": "",
+
+    // src/pages/ProjectPageEdit.vue 147
+    "Generate runtime files as a module "pytransform.py"": "",
+
+    // src/pages/ProjectPageEdit.vue 151
+    "Generate runtime files as a package "pytransform"": "",
+
+    // src/pages/ProjectPageEdit.vue 213
+    "updated": "",
+
+    // src/pages/ProjectPageEdit.vue 213
+    "created": "",
+
+    // src/pages/ProjectPageEdit.vue 218
+    "Please input project title": "",
+
+    // src/pages/ProjectInputFile.vue 8
+    "Base path for scripts, include and exclude": "",
+
+    // src/pages/ProjectInputFile.vue 14
+    "Scripts": "",
+
+    // src/pages/ProjectInputFile.vue 16
+    "Select one or more entry scripts": "",
+
+    // src/pages/ProjectInputFile.vue 29
+    "Select one entry script": "",
+
+    // src/pages/ProjectInputFile.vue 38
+    "Include": "",
+
+    // src/pages/ProjectInputFile.vue 51
+    "Exclude": "",
+
+    // src/pages/ProjectInputFile.vue 53
+    "Ignore the path and the .py files list here": "",
+
+    // src/pages/ProjectInputFile.vue 71
+    "Only the scripts list above": "",
+
+    // src/pages/ProjectInputFile.vue 75
+    "Only the ".py" files in the src path": "",
+
+    // src/pages/ProjectInputFile.vue 79
+    "All the scripts in the src path recursively": "",
+
+    // src/pages/LicensePageEdit.vue 5
+    "Edit License": "",
+
+    // src/pages/LicensePageEdit.vue 5
+    "New License": "",
+
+    // src/pages/LicensePageEdit.vue 22
+    "Leave it blank to set default name like `reg-xxxxxx`": "",
+
+    // src/pages/LicensePageEdit.vue 33
+    "Expired the obfuscated scripts": "",
+
+    // src/pages/LicensePageEdit.vue 50
+    "Enable Period Mode": "",
+
+    // src/pages/LicensePageEdit.vue 59
+    "Any ascii string data to store in the license file": "",
+
+    // src/pages/LicensePageEdit.vue 115
+    "Invalid input": "",
+
 }
 
 export default messages
-
