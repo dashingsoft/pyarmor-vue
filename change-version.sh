76c38ebe2db63e609469732a389dbf4a9cfe7d5d
Jondy Zhao <jondy.zhao@gmail.com>
2022-07-28 17:23:32 +0800

    Fix macos compatible

--- change-version.sh
+++ change-version.sh
@@ -8,11 +8,11 @@ fi
 echo "Change version to: $ver"
 for x in package.json package-lock.json; do
   echo "Patch $x"
-  sed -i -e "1,5s/[0-9].[0-9].[0-9]/$ver/" $x
+  gsed -i -e "1,5s/[0-9].[0-9].[0-9]/$ver/" $x
 done
 
 x=src/components/HomeTabAbout.vue
 echo "Patch $x"
-sed -i -e "s/<p>pyarmor-vue:.*</<p>pyarmor-vue: $ver</" $x
+gsed -i -e "s/<p>pyarmor-vue:.*</<p>pyarmor-vue: $ver</" $x
 
 git commit -m"Change version to $ver" $x package.json package-lock.json
