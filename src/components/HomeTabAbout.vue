e2b45a7b576d1af7e37d07afd2a1551d8f2bc375
Jondy Zhao <jondy.zhao@gmail.com>
2024-03-27 14:11:08 +0800

    Change version to 0.4.1

--- src/components/HomeTabAbout.vue
+++ src/components/HomeTabAbout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1 style="text-align: center">{{ $t('About') }}</h1>
-    <p>pyarmor-vue: 0.3.1</p>
+    <p>pyarmor-vue: 0.4.1</p>
     <div v-if="versionInfo.version">
       <p>pyarmor-server: {{ versionInfo.server }}</p>
       <p>Python: {{ versionInfo.python }}</p>
